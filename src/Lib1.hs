618a35b366bb68879a9f37ef8cda8f24d6fb14a1
Leonardas <leonardas.sinkevicius@mif.stud.vu.lt>
2024-09-08 13:19:32 +0000

    project setup and BNF added

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -5,4 +5,36 @@ module Lib1
 -- | This function returns a list of words
 -- to be autocompleted in your program's repl.
 completions :: [String]
-completions = []
+completions =
+  [ -- Processes
+    "harvest"
+  , "ferment"
+  , "age"
+  , "bottle"
+  , "sell"
+
+    -- Grape types
+  , "CabernetSauvignon"
+  , "Merlot"
+  , "PinotNoir"
+  , "Chardonnay"
+
+    -- Wine types
+  , "RedWine"
+  , "WhiteWine"
+  , "RoseWine"
+
+    -- Barrel types
+  , "Oak"
+  , "Steel"
+  , "Clay"
+
+    -- Units
+  , "kg"
+  , "L"
+  , "bottles"
+
+    -- Duration
+  , "days"
+  , "months"
+  ]
