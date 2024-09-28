7daf134db83ba97e14321d1043ceff4af077387a
Leonardas <leonardas.sinkevicius@mif.stud.vu.lt>
2024-09-28 11:15:52 +0000

    added more tests and functionality

--- src/Lib2.hs
+++ src/Lib2.hs
@@ -1,46 +1,50 @@
 {-# LANGUAGE InstanceSigs #-}
+
 module Lib2
-    ( Query(..),
-      GrapeType(..),
-      WineType(..),
-      Unit(..),
-      BarrelType(..),
-      Duration(..),
-      parseQuery,
-      State(..),
-      emptyState,
-      stateTransition
-    ) where
+  ( Query (..),
+    GrapeType (..),
+    WineType (..),
+    Unit (..),
+    BarrelType (..),
+    Duration (..),
+    parseQuery,
+    State (..),
+    emptyState,
+    stateTransition,
+  )
+where
 
 import Text.Read (readMaybe)
 
 -- | Data types for Queries and State
 data Query
-    = Harvest GrapeType Int Unit
-    | Ferment GrapeType Duration
-    | Age WineType Duration BarrelType
-    | Bottle WineType Int Unit
-    | Sell WineType Int Double
-    deriving (Eq, Show)
+  = Harvest GrapeType Int Unit
+  | Ferment GrapeType Duration
+  | Age WineType Duration BarrelType
+  | Bottle WineType Int Unit
+  | Sell WineType Int Double
+  | View
+  deriving (Eq, Show)
 
 data GrapeType = CabernetSauvignon | Merlot | PinotNoir | Chardonnay
-    deriving (Eq, Show)
+  deriving (Eq, Show)
 
 data WineType = RedWine | WhiteWine | RoseWine
-    deriving (Eq, Show)
+  deriving (Eq, Show)
 
 data Unit = Kg | L | Bottles
-    deriving (Eq, Show)
+  deriving (Eq, Show)
 
 data BarrelType = Oak | Steel | Clay
-    deriving (Eq, Show)
+  deriving (Eq, Show)
 
 data Duration = Days Int | Months Int
-    deriving (Eq, Show)
+  deriving (Eq, Show)
 
-data State = State { processes :: [Query] } deriving (Eq, Show)
+data State = State {processes :: [Query]} deriving (Eq, Show)
 
 -- | Parse grape type
+-- <grape_type> ::= "CabernetSauvignon" | "Merlot" | "PinotNoir" | "Chardonnay"
 parseGrapeType :: String -> Maybe GrapeType
 parseGrapeType "CabernetSauvignon" = Just CabernetSauvignon
 parseGrapeType "Merlot" = Just Merlot
@@ -49,6 +53,7 @@ parseGrapeType "Chardonnay" = Just Chardonnay
 parseGrapeType _ = Nothing
 
 -- | Parse wine type
+-- <wine_type> ::= "RedWine" | "WhiteWine" | "RoseWine"
 parseWineType :: String -> Maybe WineType
 parseWineType "RedWine" = Just RedWine
 parseWineType "WhiteWine" = Just WhiteWine
@@ -56,6 +61,7 @@ parseWineType "RoseWine" = Just RoseWine
 parseWineType _ = Nothing
 
 -- | Parse unit
+-- <unit> ::= "kg" | "L" | "bottles"
 parseUnit :: String -> Maybe Unit
 parseUnit "kg" = Just Kg
 parseUnit "L" = Just L
@@ -63,6 +69,7 @@ parseUnit "bottles" = Just Bottles
 parseUnit _ = Nothing
 
 -- | Parse barrel type
+-- <barrel_type> ::= "Oak" | "Steel" | "Clay"
 parseBarrelType :: String -> Maybe BarrelType
 parseBarrelType "Oak" = Just Oak
 parseBarrelType "Steel" = Just Steel
@@ -70,47 +77,52 @@ parseBarrelType "Clay" = Just Clay
 parseBarrelType _ = Nothing
 
 -- | Parse duration
+-- <duration> ::= <number> "days" | <number> "months"
 parseDuration :: String -> Maybe Duration
 parseDuration s = case words s of
-    [n, "days"] -> Days <$> readMaybe n
-    [n, "months"] -> Months <$> readMaybe n
-    _ -> Nothing
+  [n, "days"] -> Days <$> readMaybe n
+  [n, "months"] -> Months <$> readMaybe n
+  _ -> Nothing
 
 -- | Parses user's input.
 parseQuery :: String -> Either String Query
 parseQuery input =
-    case words (filter (`notElem` "(),") input) of
-        -- Harvest
-        ("harvest" : g : n : u : _) -> 
-            case (parseGrapeType g, readMaybe n :: Maybe Int, parseUnit u) of
-                (Just grape, Just quantity, Just unit) -> Right (Harvest grape quantity unit)
-                _ -> Left "Failed to parse harvest"
-        -- Ferment
-        ("ferment" : g : n : d : _) ->
-            case (parseGrapeType g, parseDuration (n ++ " " ++ d)) of
-                (Just grape, Just duration) -> Right (Ferment grape duration)
-                _ -> Left "Failed to parse ferment"
-        -- Age
-        ("age" : w : n : d : b : _) ->
-            case (parseWineType w, parseDuration (n ++ " " ++ d), parseBarrelType b) of
-                (Just wine, Just duration, Just barrel) -> Right (Age wine duration barrel)
-                _ -> Left "Failed to parse age"
-        -- Bottle
-        ("bottle" : w : n : u : _) ->
-            case (parseWineType w, readMaybe n :: Maybe Int, parseUnit u) of
-                (Just wine, Just quantity, Just unit) -> Right (Bottle wine quantity unit)
-                _ -> Left "Failed to parse bottle"
-        -- Sell
-        ("sell" : w : n : p : _) ->
-            case (parseWineType w, readMaybe n :: Maybe Int, readMaybe p :: Maybe Double) of
-                (Just wine, Just quantity, Just price) -> Right (Sell wine quantity price)
-                _ -> Left "Failed to parse sell"
-        _ -> Left "Unknown command"
+  case words (filter (`notElem` "(),") input) of
+    -- <harvest> ::= "harvest" "(" <grape_type> "," <quantity> ")"
+    ("harvest" : g : n : u : _) ->
+      case (parseGrapeType g, readMaybe n :: Maybe Int, parseUnit u) of
+        (Just grape, Just quantity, Just unit) -> Right (Harvest grape quantity unit)
+        _ -> Left "Failed to parse harvest"
+    -- <ferment> ::= "ferment" "(" <grape_type> "," <duration> ")"
+    ("ferment" : g : n : d : _) ->
+      case (parseGrapeType g, parseDuration (n ++ " " ++ d)) of
+        (Just grape, Just duration) -> Right (Ferment grape duration)
+        _ -> Left "Failed to parse ferment"
+    -- <age> ::= "age" "(" <wine_type> "," <duration> "," <barrel_type> ")"
+    ("age" : w : n : d : b : _) ->
+      case (parseWineType w, parseDuration (n ++ " " ++ d), parseBarrelType b) of
+        (Just wine, Just duration, Just barrel) -> Right (Age wine duration barrel)
+        _ -> Left "Failed to parse age"
+    -- <bottle> ::= "bottle" "(" <wine_type> "," <quantity> ")"
+    ("bottle" : w : n : u : _) ->
+      case (parseWineType w, readMaybe n :: Maybe Int, parseUnit u) of
+        (Just wine, Just quantity, Just unit) -> Right (Bottle wine quantity unit)
+        _ -> Left "Failed to parse bottle"
+    -- <sell> ::= "sell" "(" <wine_type> "," <quantity> "," <price> ")"
+    ("sell" : w : n : p : _) ->
+      case (parseWineType w, readMaybe n :: Maybe Int, readMaybe p :: Maybe Double) of
+        (Just wine, Just quantity, Just price) -> Right (Sell wine quantity price)
+        _ -> Left "Failed to parse sell"
+    -- <view> ::= "view"
+    ("view" : _) -> Right View
+    _ -> Left "Unknown command"
 
 -- | Initial program state.
 emptyState :: State
-emptyState = State { processes = [] }
+emptyState = State {processes = []}
 
 -- | Transition the state with a new query.
 stateTransition :: State -> Query -> Either String (Maybe String, State)
-stateTransition st query = Right (Just (show query), st { processes = query : processes st })
+stateTransition st query = case query of
+  View -> Right (Just (show st), st)
+  _ -> Right (Just (show query), st {processes = query : processes st})
--- test/Spec.hs
+++ test/Spec.hs
@@ -20,6 +20,21 @@ unitTests = testGroup "Lib2 tests"
       Lib2.parseQuery "ferment (Merlot, 30 days)" @?= 
         Right (Lib2.Ferment Lib2.Merlot (Lib2.Days 30)),
         
+    testCase "Age command parsing" $
+      Lib2.parseQuery "age (RedWine, 6 months, Oak)" @?= 
+        Right (Lib2.Age Lib2.RedWine (Lib2.Months 6) Lib2.Oak),
+
+    testCase "Bottle command parsing" $
+      Lib2.parseQuery "bottle (WhiteWine, 50, bottles)" @?= 
+        Right (Lib2.Bottle Lib2.WhiteWine 50 Lib2.Bottles),
+
+    testCase "Sell command parsing" $
+      Lib2.parseQuery "sell (RoseWine, 20, 15.99)" @?= 
+        Right (Lib2.Sell Lib2.RoseWine 20 15.99),
+
+    testCase "View command parsing" $
+      Lib2.parseQuery "view" @?= Right Lib2.View,
+
     testCase "Invalid command parsing" $
       Lib2.parseQuery "invalid command" @?= Left "Unknown command",
         
@@ -30,5 +45,11 @@ unitTests = testGroup "Lib2 tests"
             case Lib2.stateTransition initialState query of
                 Right (_, newState) -> Lib2.processes newState @?= [query]
                 Left err -> error err
-        Left err -> error err
+        Left err -> error err,
+
+    testCase "State transition with View" $
+      let initialState = Lib2.emptyState
+      in case Lib2.stateTransition initialState Lib2.View of
+          Right (Just stateView, _) -> stateView @?= show initialState
+          Left err -> error err
   ]
